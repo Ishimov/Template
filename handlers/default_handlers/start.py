17c853a8b3498c59d0463ada2f14be7f24fef271
Nikita Ishimov <111559693+Ishimov@users.noreply.github.com>
2023-04-05 11:30:43 +0500

    Update start.py

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -3,7 +3,7 @@ from aiogram.types import Message
 from loader import dp
 
 @dp.message_handler(commands=['start'])
-async def cmd_help(message: Message) -> None:
+async def cmd_start(message: Message) -> None:
     await message.answer(
         text=f'Добро пожаловать, {message.from_user.full_name}!'
         )
